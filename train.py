4cb4c54d7c9b21a84c4f59f9349493b1dd673a8d
Addanki Asish <54019861+Asish-08@users.noreply.github.com>
2021-07-10 19:42:15 +0530

    Add files via upload

---
+++ train.py
@@ -0,0 +1,71 @@
+from keras.optimizers import RMSprop
+from keras.preprocessing.image import ImageDataGenerator
+import cv2
+from keras.models import Sequential
+from keras.layers import Conv2D, Input, ZeroPadding2D, BatchNormalization, Activation, MaxPooling2D, Flatten, Dense,Dropout
+from keras.models import Model, load_model
+from keras.callbacks import TensorBoard, ModelCheckpoint
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score
+from sklearn.utils import shuffle
+import imutils
+import numpy as np
+import matplotlib.pyplot as plt
+
+model =Sequential([
+    Conv2D(100, (3,3), activation='relu', input_shape=(150, 150, 3)),
+    MaxPooling2D(2,2),
+    
+    Conv2D(100, (3,3), activation='relu'),
+    MaxPooling2D(2,2),
+    
+    Flatten(),
+    Dropout(0.5),
+    Dense(50, activation='relu'),
+    Dense(2, activation='softmax')
+])
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+
+TRAINING_DIR = "./train"
+train_datagen = ImageDataGenerator(rescale=1.0/255,
+                                   rotation_range=40,
+                                   width_shift_range=0.2,
+                                   height_shift_range=0.2,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True,
+                                   fill_mode='nearest')
+
+train_generator = train_datagen.flow_from_directory(TRAINING_DIR, 
+                                                    batch_size=30, 
+                                                    target_size=(150, 150))
+VALIDATION_DIR = "./test"
+validation_datagen = ImageDataGenerator(rescale=1.0/255)
+
+validation_generator = validation_datagen.flow_from_directory(VALIDATION_DIR, 
+                                                         batch_size=30, 
+                                                         target_size=(150, 150))
+checkpoint = ModelCheckpoint('model2-{epoch:03d}.model',monitor='val_loss',verbose=0,save_best_only=True,mode='auto')
+
+
+history = model.fit_generator(train_generator,
+                              epochs=15,
+                              validation_data=validation_generator,
+                              callbacks=[checkpoint])
+print(history.history.keys())
+# summarize history for accuracy
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
